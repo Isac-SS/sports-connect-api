67cc0948e73977513da044c860d8e552aa4710f4
Isac-SS <isacsants679@gmail.com>
2025-05-09 15:07:10 -0300

    feat(app): estabelece conexäo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import express from "express";
 import cors from "cors";
+import db from "./config/dbConnect.js";
+
+db.on("error", console.log.bind(console, 'Erro de conexão!'));
+db.once("open", () => console.log("Conexão com o banco feita com sucesso!"));
 
 const app =  express();
 app.use(cors());
